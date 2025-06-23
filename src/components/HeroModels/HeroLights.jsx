1f52a97cb5e49cbcb626d8a2d406dd1c2ce4683e
notoamrmustafa <omarmustafa2024@gmail.com>
2025-06-23 09:56:10 -0400

    added a new room

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -7,7 +7,7 @@ const HeroLights = () => {
                 position={[2, 5, 6]}
                 angle={0.15}
                 intensity={100}
-                penumbra={0.2}
+                penumbra={0}
                 color="white"
             />
 
@@ -15,20 +15,20 @@ const HeroLights = () => {
                 position={[4, 5, 4]}
                 angle={0.3}
                 intensity={40}
-                penumbra={0.5}
-                color="#4cc9f0"
+                penumbra={0}
+                color="#fff"
             />
 
             <spotLight
                 position={[-3, 5, 5]}
                 angle={0.4}
                 intensity={60}
-                penumbra={1}
-                color="#9d4edd"
+                penumbra={0}
+                color="#fffff4"
             />
 
             <primitive
-                object={new THREE.RectAreaLight('#A259FF', 8, 3, 2)}
+                object={new THREE.RectAreaLight('#fffff4', 8, 3, 2)}
                 position={[1, 3, 4]}
                 intensity={15}
                 rotation={[-Math.PI / 4, Math.PI / 4, 0]}
@@ -37,13 +37,13 @@ const HeroLights = () => {
             <pointLight
                 position={[0, 1, 0]}
                 intensity={10}
-                color="#7209b7"
+                color="#fffff4"
             />
 
             <pointLight
                 position={[1, 2, -2]}
                 intensity={10}
-                color="#0d00a4"
+                color="#fffff4"
             />
         </>
     )
