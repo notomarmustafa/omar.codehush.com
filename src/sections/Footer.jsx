fa9ab79d649bdd698434eae1ed0f49494a07fcaa
notoamrmustafa <omarmustafa2024@gmail.com>
2025-11-16 02:35:13 -0500

    init

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,28 +1,59 @@
-import React from 'react'
-import {socialImgs} from "../constants/index.js";
+import React from 'react';
+import { socialImgs } from "../constants/index.js";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-container">
-                <div className="flex flex-col justify-center md:items-start items-center">
-                    <a href="https://codehush.com/termsofservice" className="underline" target="_blank">Terms and Conditions</a>
-                </div>
-                <div className="socials">
-                    {socialImgs.map((img) => (
-                        <a className="icon" target="_blank" href={img.url} key={img.url}>
-                            <img src={img.imgPath} />
-                        </a>
-                    ))}
-                </div>
+  const { t } = useTranslation();
 
-                <div className="flex flex-col justify-center">
-                    <p className="text-center md:text-end">
-                        &#169; {new Date().getFullYear()} Omar | <a href="https://codehush.com" target="_blank" className="underline">CodeHush</a>. All rights reserved.
-                    </p>
-                </div>
-            </div>
-        </footer>
-    )
-}
-export default Footer
+  return (
+    <footer className="footer">
+      <div className="footer-container flex flex-col md:flex-row justify-between items-center gap-5 py-5 px-5">
+
+        {/* Terms & Conditions */}
+        <div className="flex flex-col justify-center md:items-start items-center">
+          <a
+            href="https://codehush.com/termsofservice"
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("footer_terms")}
+          </a>
+        </div>
+
+        {/* Social Icons */}
+        <div className="socials flex gap-4">
+          {socialImgs.map((img) => (
+            <a
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={img.url}
+              key={img.url}
+            >
+              <img src={img.imgPath} alt={img.name} className="w-6 h-6" />
+            </a>
+          ))}
+        </div>
+
+        {/* Copyright */}
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            &#169; {new Date().getFullYear()} Omar |{" "}
+            <a
+              href="https://codehush.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              CodeHush
+            </a>. {t("footer_text")}
+          </p>
+        </div>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
